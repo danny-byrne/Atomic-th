e0d92050276610a747f0e758fb84f8a8a4ef2ada
Danny Byrne <danny.byrne.dev@gmail.com>
2023-10-14 18:28:58 -0700

    add tailwind stylings

--- components/TransactionCard.tsx
+++ components/TransactionCard.tsx
@@ -1,11 +1,24 @@
-const PendingTag = <div>Pending</div>;
+import Card from "./icons/Card";
+
+const pendingTag = (
+  <div className="flex items-center justify-center w-20 h-7 bg-yellow-100 rounded-xl border border-zinc-400">
+    <div className="w-16 h-3.5 text-center text-black  text-xs font-normal font-['Arial']">
+      PENDING
+    </div>
+  </div>
+);
+
+enum purchaseDirection {
+  Credit = "Credit",
+  Debit = "Debit",
+}
 
 export interface ITransaction {
   amountCents: number;
   cardLast4Digits: string;
   createdAt: string; // This should be a valid date string, consider using Date type if you want to parse it.
   description: string;
-  direction: "Credit" | "Debit"; // Assuming direction can only be "Credit" or "Debit"
+  direction: purchaseDirection; // Assuming direction can only be "Credit" or "Debit"
   id: number;
   merchantName: string;
   status: "settled" | "pending"; // Define all possible status values
@@ -15,6 +28,17 @@ export interface ITransaction {
 export interface ITransactionProps {
   transaction: ITransaction;
 }
+
+const convertAmountCentsToTransactionDisplay = (
+  amount: number,
+  direction: purchaseDirection
+) => {
+  const dollars = Math.floor(amount / 100);
+  const cents = amount % 100;
+  const formattedCents = cents < 10 ? `0${cents}` : `${cents}`;
+  const prefix = direction === purchaseDirection.Credit ? "-" : "";
+  return `$${prefix}${dollars}.${formattedCents}`;
+};
 
 const TransactionCard: React.FC<ITransactionProps> = ({
   transaction,
@@ -26,25 +50,40 @@ const TransactionCard: React.FC<ITransactionProps> = ({
     description,
     status,
     updatedAt,
+    direction,
   } = transaction;
 
   const formattedCreatedAtDate = createdAt; // || updatedAt
-  //if direction is 'credit' append a '-' in front;
-  const transactionCost = amountCents;
-  const cardClass = "w-96 h-32 bg-orange-100 rounded-lg shadow";
-  const flexClass = "w-full flex flex-row justify-between";
+
+  const cardClass =
+    "h-32 flex flex-col justify-center pl-10 pr-10 bg-orange-100 border-zinc-800 rounded-lg shadow ";
+  const flexRowClass = "flex flex-row justify-between align-middle";
+  const transactionClass =
+    "flex flex-row w-1/4  justify-between bg-red border-zinc-800 items-center";
+  const statusIsPending = status === "pending";
+  const transactionAmount = convertAmountCentsToTransactionDisplay(
+    amountCents,
+    direction
+  );
 
   return (
     <div className={cardClass}>
-      <div className={flexClass}>
-      <div className="flex flex-col">
-        <div className="flex flex-row gap-4">
-          {description}
-          {PendingTag}
+      <div className={flexRowClass}>
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-4">
+            {description}
+
+            {statusIsPending && pendingTag}
+          </div>
+          {formattedCreatedAtDate}
         </div>
-        {formattedCreatedAtDate}
-      </div>
-      <span>{transactionCost}</span>
+        <div className={transactionClass}>
+          <div className="w-2/5 flex flex-row justify-between">
+            <Card />
+            <span>{"x" + cardLast4Digits}</span>
+          </div>
+          <span>{transactionAmount}</span>
+        </div>
       </div>
     </div>
   );
